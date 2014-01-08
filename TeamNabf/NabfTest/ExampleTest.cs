81804708854b7285aa9f86a540e7c1783f566b68
jannick <jannickboese@msn.com>
2014-01-08 14:16:29 +0100

    Addded test project

---
+++ TeamNabf/NabfTest/ExampleTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NabfTest
+{
+	[TestFixture]
+	public class ExampleTest
+	{
+
+		[Test]
+		public void WhatIsBeingTested_CurrentState_ExpectedResult()
+		{
+			Assert.Pass();
+		}
+	}
+}
