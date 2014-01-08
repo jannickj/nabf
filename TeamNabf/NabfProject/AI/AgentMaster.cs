dbbe2eed13d326e9b93265beebc17073adb6e547
jannick <jannickboese@msn.com>
2014-01-08 14:32:14 +0100

    Added nunit by request of and

---
+++ TeamNabf/NabfProject/AI/AgentMaster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XmasEngineController.AI;
+using XmasEngineModel;
+
+namespace NabfProject.AI
+{
+	public class AgentMaster : AgentManager
+	{
+		
+
+		protected override Func<KeyValuePair<string, AgentController>> AquireAgentControllerContructor()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
