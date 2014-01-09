229a047c28deeeba4ce83ace703c64a776405249
Nicolai Christensen <nicolai_christensen@hotmail.com>
2014-01-09 09:39:26 +0100

    Created first send message in agent-server communication.

---
+++ TeamNabf/NabfProject/AI/ServerCommunication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace NabfProject.AI
+{
+    public class ServerCommunication
+    {
+        private XmlWriter stream;
+        private XmlSerializer xmlSer;
+
+        public ServerCommunication(XmlWriter stream, XmlSerializer xmlSer)
+        {
+            // TODO: Complete member initialization
+            this.stream = stream;
+            this.xmlSer = xmlSer;
+        }
+        
+        public void SendMessage(ServerMessages.ServerMessage message)
+        {
+            stream.WriteStartDocument(false);
+            xmlSer.Serialize(stream, message);
+        }
+    }
+}
