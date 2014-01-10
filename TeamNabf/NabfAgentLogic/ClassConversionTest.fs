49a26ca593a0c91db39b9009db96b768574f0dfd
Søren Jacobsen <srnjcbsn@gmail.com>
2014-01-10 11:42:57 +0100

    Started implementing iilang in Fsharp, untested, incomplete

---
+++ TeamNabf/NabfAgentLogic/ClassConversionTest.fs
@@ -0,0 +1,26 @@
+open JSLibrary.IiLang
+open System.Xml.Serialization
+
+type Element = ParameterList of Element list
+             | Function      of (string * Element)
+             | Numeral       of float
+             | Identifier    of string
+
+type Data = Percept of (string * Element)
+          | Action  of (string * Element)
+
+type PerceptCollection = Data list
+
+let rec evalIil (iil : IilParameter) = 
+    match iil with
+    | :? Parameters.IilParameterList as paramlist 
+        -> ParameterList (List.ofSeq paramlist.Parameters |> List.map evalIil)
+    | :? Parameters.IilFunction as func           
+        -> Function (func.Name, ParameterList (List.ofSeq func.Parameters |> List.map evalIil))
+    | :? Parameters.IilIdentifier as id           
+        -> Identifier id.Value
+    | :? Parameters.IilNumeral as num             
+        -> Numeral num.Value
+    | _ -> failwith "the object %O is not a recognized iilang element (IilElement)"
+
+   
