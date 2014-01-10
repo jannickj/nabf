b39aab50ef8d2fde7b8b7f0604e28b010fb9a788
jannick <jannickboese@msn.com>
2014-01-10 16:10:23 +0100

    Begun implementation of interface for agentlogic

---
+++ TeamNabf/NabfAgentLogic/AgentLogicClient.fs
@@ -0,0 +1,18 @@
+﻿namespace NabfAgentLogic
+    open NabfAgentLogic.AgentInterfaces;
+    open JSLibrary.Data.GenericEvents;
+    open JSLibrary.IiLang;
+    open JSLibrary.IiLang.DataContainers;
+
+    type AgentLogicClient() = 
+        
+        let JobCreatedEvent = new Event<UnaryValueHandler<IilAction>, UnaryValueEvent<IilAction>>()
+        
+        interface IAgentLogic with
+            member this.EvaluateJob(id) = (null,false)
+            member this.HandlePercepts(percepts) = ()
+            member this.GetJobs = [(1,1)]
+            member this.Start = ()
+            member this.CurrentDecision = null
+            [<CLIEvent>]
+            member this.JobCreated = JobCreatedEvent.Publish
