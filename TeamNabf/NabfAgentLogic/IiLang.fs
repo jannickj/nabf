3f7727883e9b641dd716775842d6bed8360a4086
Søren Jacobsen <srnjcbsn@gmail.com>
2014-01-13 17:42:09 +0100

    began implmenting creating C# iil objects from F#, needs JSLibrary update

--- TeamNabf/NabfAgentLogic/IiLang.fs
+++ TeamNabf/NabfAgentLogic/IiLang.fs
@@ -1,10 +1,11 @@
 namespace IiLang
     module IiLangHandler =
         open JSLibrary.IiLang
+        open JSLibrary.IiLang.Parameters
         open System.Xml.Serialization
 
         type Element = ParameterList of Element list
-                     | Function      of (string * Element)
+                     | Function      of (string * Element list)
                      | Numeral       of float
                      | Identifier    of string
 
@@ -13,12 +14,27 @@ namespace IiLang
 
         type PerceptCollection = Data list
 
+        let rec buildIil (data : Element) : IilElement = 
+            match data with
+            | ParameterList ls -> 
+                new IilParameterList (List.map buildIil ls |> List.toArray) 
+                :> IilElement
+            | Function (name, ls) -> 
+                new IilFunction (name, List.map buildIil ls |> List.toArray) 
+                :> IilElement
+            | Numeral num -> 
+                new IilNumeral (num) 
+                :> IilElement
+            | Identifier str -> 
+                new IilIdentifier (str) 
+                :> IilElement
+
         let rec evalIil (iil : IilParameter) = 
             match iil with
             | :? Parameters.IilParameterList as paramlist 
                 -> ParameterList (List.ofSeq paramlist.Parameters |> List.map evalIil)
             | :? Parameters.IilFunction as func           
-                -> Function (func.Name, ParameterList (List.ofSeq func.Parameters |> List.map evalIil))
+                -> Function (func.Name, List.ofSeq func.Parameters |> List.map evalIil)
             | :? Parameters.IilIdentifier as id           
                 -> Identifier id.Value
             | :? Parameters.IilNumeral as num             
