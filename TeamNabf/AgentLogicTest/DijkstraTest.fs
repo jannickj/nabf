0c04265080abd3a49902ede7ff5341736fdb21d9
PhilipOrum <philip.oerum@gmail.com>
2014-01-14 14:56:45 +0100

    Added test to show that Dijkstra should not just take the path with the lowest individual edge costs, but the path with the lowest total cost.

--- TeamNabf/AgentLogicTest/DijkstraTest.fs
+++ TeamNabf/AgentLogicTest/DijkstraTest.fs
@@ -13,8 +13,8 @@ module DijkstraTest =
             member this.FindPath_SimpleGraph_FindCorrectPath () =
 
                 let graph = [ ("a", { Identifier = "a"; Value = None; Edges = [(None, "c"); (None, "b")] |> Set.ofList}); 
-                                        ("b", { Identifier = "b"; Value = None; Edges = [(None, "c"); (None, "a")] |> Set.ofList}); 
-                                        ("c", { Identifier = "c"; Value = None; Edges = [(None, "a"); (None, "b")] |> Set.ofList}) ] |> Map.ofList
+                              ("b", { Identifier = "b"; Value = None; Edges = [(None, "c"); (None, "a")] |> Set.ofList}); 
+                              ("c", { Identifier = "c"; Value = None; Edges = [(None, "a"); (None, "b")] |> Set.ofList}) ] |> Map.ofList
                 let correctPath = Some ["c"]
 
                 let actualPath = Dijkstra.dijkstra graph.["a"] graph.["c"] graph
@@ -24,22 +24,21 @@ module DijkstraTest =
             [<Test>]
             member this.FindPath_AlreadyThere_ReturnEmptyPath () =
 
-                let graph = [ ("a", { Identifier = "a"; Value = None; Edges = [(None, "c"); (None, "b")] |> Set.ofList})
-                            ; ("b", { Identifier = "b"; Value = None; Edges = [(None, "c"); (None, "a")] |> Set.ofList})
-                            ; ("c", { Identifier = "c"; Value = None; Edges = [(None, "a"); (None, "b")] |> Set.ofList}) 
+                let graph = [ ("a", { Identifier = "a"; Value = None; Edges = [(None, "c"); (None, "b")] |> Set.ofList}); 
+                              ("b", { Identifier = "b"; Value = None; Edges = [(None, "c"); (None, "a")] |> Set.ofList});
+                              ("c", { Identifier = "c"; Value = None; Edges = [(None, "a"); (None, "b")] |> Set.ofList}) 
                             ] |> Map.ofList
 
-                let correctPath = Some []
+                let path = Dijkstra.dijkstra graph.["a"] graph.["a"] graph
 
-                let actualPath = Dijkstra.dijkstra graph.["a"] graph.["a"] graph
-
-                Assert.AreEqual (correctPath, actualPath)
+                // Verify that the result is Some []
+                Assert.IsEmpty(path.Value)
 
             [<Test>]
             member this.FindPath_NoPathExists_ReturnsNone () =
 
-                let graph = [ ("a", { Identifier = "a"; Value = None; Edges = [] |> Set.ofList})
-                            ; ("b", { Identifier = "b"; Value = None; Edges = [] |> Set.ofList}) 
+                let graph = [ ("a", { Identifier = "a"; Value = None; Edges = [] |> Set.ofList});
+                              ("b", { Identifier = "b"; Value = None; Edges = [] |> Set.ofList}) 
                             ] |> Map.ofList
                 let correctOutput = None
 
@@ -57,10 +56,10 @@ module DijkstraTest =
                //    A-------D------E
 
                 let graph = [ ("a", { Identifier = "a"; Value = None; Edges = [(None, "b"); (None, "d")] |> Set.ofList}); 
-                                        ("b", { Identifier = "b"; Value = None; Edges = [(None, "a"); (None, "c")] |> Set.ofList}); 
-                                        ("c", { Identifier = "c"; Value = None; Edges = [(None, "b"); (None, "e")] |> Set.ofList}); 
-                                        ("d", { Identifier = "d"; Value = None; Edges = [(None, "a"); (None, "e")] |> Set.ofList}); 
-                                        ("e", { Identifier = "e"; Value = None; Edges = [(None, "c"); (None, "d")] |> Set.ofList}); ] |> Map.ofList
+                              ("b", { Identifier = "b"; Value = None; Edges = [(None, "a"); (None, "c")] |> Set.ofList}); 
+                              ("c", { Identifier = "c"; Value = None; Edges = [(None, "b"); (None, "e")] |> Set.ofList}); 
+                              ("d", { Identifier = "d"; Value = None; Edges = [(None, "a"); (None, "e")] |> Set.ofList}); 
+                              ("e", { Identifier = "e"; Value = None; Edges = [(None, "c"); (None, "d")] |> Set.ofList}); ] |> Map.ofList
 
                 let correctOutput = Some ["d";"e"]
 
@@ -92,14 +91,14 @@ module DijkstraTest =
                             }
 
                 let graph = [ ("a", { Identifier = "a"; Value = None; Edges = [(Some 10, "b"); (Some 1, "d")] |> Set.ofList}); 
-                                        ("b", { Identifier = "b"; Value = None; Edges = [(Some 10, "a"); (Some 10, "c")] |> Set.ofList}); 
-                                        ("c", { Identifier = "c"; Value = None; Edges = [(Some 10, "b"); (Some 10, "g")] |> Set.ofList}); 
-                                        ("d", { Identifier = "d"; Value = None; Edges = [(Some 1, "a"); (Some 1, "e")] |> Set.ofList}); 
-                                        ("e", { Identifier = "e"; Value = None; Edges = [(Some 1, "d"); (Some 1, "f")] |> Set.ofList});
-                                        ("f", { Identifier = "f"; Value = None; Edges = [(Some 1, "e"); (Some 1, "g")] |> Set.ofList});
-                                        ("g", { Identifier = "g"; Value = None; Edges = [(Some 1, "f"); (Some 10, "c")] |> Set.ofList}); ] |> Map.ofList
+                              ("b", { Identifier = "b"; Value = None; Edges = [(Some 10, "a"); (Some 10, "c")] |> Set.ofList}); 
+                              ("c", { Identifier = "c"; Value = None; Edges = [(Some 10, "b"); (Some 10, "g")] |> Set.ofList}); 
+                              ("d", { Identifier = "d"; Value = None; Edges = [(Some 1, "a"); (Some 1, "e")] |> Set.ofList}); 
+                              ("e", { Identifier = "e"; Value = None; Edges = [(Some 1, "d"); (Some 1, "f")] |> Set.ofList});
+                              ("f", { Identifier = "f"; Value = None; Edges = [(Some 1, "e"); (Some 1, "g")] |> Set.ofList});
+                              ("g", { Identifier = "g"; Value = None; Edges = [(Some 1, "f"); (Some 10, "c")] |> Set.ofList}); ] |> Map.ofList
 
-                let correctOutput = Some ["d","e","f","g"]
+                let correctOutput = Some ["d";"e";"f";"g"]
 
                 let actualOutput = Dijkstra.dijkstra graph.["a"] graph.["g"] graph
 
@@ -127,11 +126,11 @@ module DijkstraTest =
                             }
 
                 let graph1 = [ ("a", { Identifier = "a"; Value = None; Edges = [(Some 2, "b"); (Some 1, "c")] |> Set.ofList}); 
-                                        ("b", { Identifier = "b"; Value = None; Edges = [(Some 2, "a"); (Some 2, "d")] |> Set.ofList}); 
-                                        ("c", { Identifier = "c"; Value = None; Edges = [(Some 1, "a"); (Some 1, "d")] |> Set.ofList}); 
-                                        ("d", { Identifier = "d"; Value = None; Edges = [(Some 2, "b"); (Some 1, "c")] |> Set.ofList}); ] |> Map.ofList
+                               ("b", { Identifier = "b"; Value = None; Edges = [(Some 2, "a"); (Some 2, "d")] |> Set.ofList}); 
+                               ("c", { Identifier = "c"; Value = None; Edges = [(Some 1, "a"); (Some 1, "d")] |> Set.ofList}); 
+                               ("d", { Identifier = "d"; Value = None; Edges = [(Some 2, "b"); (Some 1, "c")] |> Set.ofList}); ] |> Map.ofList
 
-                let correctOutput1 = Some ["c","d"]
+                let correctOutput1 = Some ["c";"d"]
 
                 let actualOutput1 = Dijkstra.dijkstra graph1.["a"] graph1.["d"] graph1
 
@@ -160,12 +159,48 @@ module DijkstraTest =
                             }
 
                 let graph2 = [ ("a", { Identifier = "a"; Value = None; Edges = [(Some 1, "b"); (Some 2, "c")] |> Set.ofList}); 
-                                        ("b", { Identifier = "b"; Value = None; Edges = [(Some 1, "a"); (Some 1, "d")] |> Set.ofList}); 
-                                        ("c", { Identifier = "c"; Value = None; Edges = [(Some 2, "a"); (Some 2, "d")] |> Set.ofList}); 
-                                        ("d", { Identifier = "d"; Value = None; Edges = [(Some 1, "b"); (Some 2, "c")] |> Set.ofList}); ] |> Map.ofList
+                               ("b", { Identifier = "b"; Value = None; Edges = [(Some 1, "a"); (Some 1, "d")] |> Set.ofList}); 
+                               ("c", { Identifier = "c"; Value = None; Edges = [(Some 2, "a"); (Some 2, "d")] |> Set.ofList}); 
+                               ("d", { Identifier = "d"; Value = None; Edges = [(Some 1, "b"); (Some 2, "c")] |> Set.ofList}); ] |> Map.ofList
 
-                let correctOutput = Some ["b","d"]
+                let correctOutput = Some ["b";"d"]
 
                 let actualOutput = Dijkstra.dijkstra graph2.["a"] graph2.["d"] graph2
 
                 Assert.AreEqual (correctOutput, actualOutput)
+
+            [<Test>]
+            member this.FindPath_PathWithLowestCostsIsLongest_ReturnsShortestPath () =
+           
+                //          
+                //             2
+                //     B---------------C         
+                //     |                |       Find a path
+                //   2 |                | 2     from A to G
+                //     |                |
+                //     A---D---E---F---G
+                //       1   1   1   1
+
+                let agent = {   
+                                Id = "a1";
+                                Type = Explorer;
+                                Energy = 10;
+                                Health = 1;
+                                Strength = 1;
+                                VisionRange = 1
+                            }
+
+                let graph = [ ("a", { Identifier = "a"; Value = None; Edges = [(Some 2, "b"); (Some 1, "d")] |> Set.ofList}); 
+                              ("b", { Identifier = "b"; Value = None; Edges = [(Some 2, "a"); (Some 2, "c")] |> Set.ofList}); 
+                              ("c", { Identifier = "c"; Value = None; Edges = [(Some 2, "b"); (Some 2, "g")] |> Set.ofList}); 
+                              ("d", { Identifier = "d"; Value = None; Edges = [(Some 1, "a"); (Some 1, "e")] |> Set.ofList}); 
+                              ("e", { Identifier = "e"; Value = None; Edges = [(Some 1, "d"); (Some 1, "f")] |> Set.ofList});
+                              ("f", { Identifier = "f"; Value = None; Edges = [(Some 1, "e"); (Some 1, "g")] |> Set.ofList});
+                              ("g", { Identifier = "g"; Value = None; Edges = [(Some 1, "f"); (Some 2, "c")] |> Set.ofList}); ] |> Map.ofList
+
+                let correctOutput = Some ["b";"c";"g"]
+
+                let actualOutput = Dijkstra.dijkstra graph.["a"] graph.["g"] graph
+
+                Assert.AreEqual (correctOutput,actualOutput)
+            
