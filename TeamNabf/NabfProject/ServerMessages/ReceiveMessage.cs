bfb15fc89fa0c9874af081396c26278bf821518a
Nicolai Christensen <nicolai_christensen@hotmail.com>
2014-01-14 10:02:08 +0100

    i hope this works...

--- TeamNabf/NabfProject/ServerMessages/ReceiveMessage.cs
+++ TeamNabf/NabfProject/ServerMessages/ReceiveMessage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NabfProject.AI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,33 +8,47 @@ using System.Xml.Serialization;
 
 namespace NabfProject.ServerMessages
 {
-    [XmlRoot("message")]
-    public class ReceiveMessage : IXmlSerializable
+    public class ReceiveMessage : XmlTransmitterMessage<InternalReceiveMessage>
     {
-        public InternalReceiveMessage Message { get; private set; }
         public long Timestamp { get; private set; }
         public string Type { get; private set; }
-
-        public System.Xml.Schema.XmlSchema GetSchema()
-        {
-            throw new NotImplementedException();
-        }
-
+        
         public void ReadXml(System.Xml.XmlReader reader)
         {
-            reader.MoveToContent();
-            var message = ServerMessageFactory.Instance.ConstructMessage(reader["type"]);
-            var isEmpty = reader.IsEmptyElement;
+            //reader.MoveToContent();
+            //var message = ServerMessageFactory.Instance.ConstructMessage(reader["type"]);
+            //var isEmpty = reader.IsEmptyElement;
 
+            //Timestamp = Convert.ToInt64(reader["timestamp"]);
+            //Type = reader["type"];
+
+            //reader.Read();
+            
+            //this.Message = message;
+            //message.ReadXml(reader);
+            //if(!isEmpty)
+            //    reader.ReadEndElement();
+
+
+        }
+
+        public override InternalReceiveMessage ConstructMessage(System.Xml.XmlReader reader)
+        {
+            return ServerMessageFactory.Instance.ConstructMessage(Type);
+        }
+
+        public override void ReadNode(System.Xml.XmlReader reader)
+        {
             Timestamp = Convert.ToInt64(reader["timestamp"]);
             Type = reader["type"];
+        }
 
-            reader.Read();
-            
-            this.Message = message;
-            message.ReadXml(reader);
-            if(!isEmpty)
-                reader.ReadEndElement();
+        public override string NodeName
+        {
+            get
+            {
+                return "message";
+            }
         }
 
         public void WriteXml(System.Xml.XmlWriter writer)
