086a9b469a742984710aab650ee9a4d23fa38dc1
And <andreas.spejder@gmail.com>
2014-01-14 15:39:37 +0100

    Initial commit of KnowledgeManager

---
+++ TeamNabf/NabfProject/KnowledgeManager/KnowledgeManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JSLibrary.IiLang;
+using JSLibrary.IiLang.Parameters;
+using NabfProject;
+using NabfProject.AI;
+
+namespace NabfProject.KnowledgeManager
+{
+    public class KnowledgeManager
+    {
+        private List<NabfAgent> _sharingList = new List<NabfAgent>();
+
+        private List<Knowledge> _knowledgeBase = new List<Knowledge>();
+
+        public void Subscribe(NabfAgent agent)
+        {
+            _sharingList.Add(agent);
+        }
+        public bool Unsubscribe(NabfAgent agent)
+        {
+            return _sharingList.Remove(agent);
+        }
+
+        public void SendPercept(IilFunction iilfunc, NabfAgent agent)
+        {
+            foreach (IilParameter param in iilfunc.Parameters)
+            {
+
+            }
+        }
+    }
+
+    public interface Knowledge : IEquatable<Knowledge>
+    {
+
+    }
+
+}
