437f365848ee23d46c5aa874bc746cefb4e23069
jannick <jannickboese@msn.com>
2014-01-23 15:25:29 +0100

    Added zone position decider

--- TeamNabf/NabfAgentLogic/logic/ExplorerLogic.fs
+++ TeamNabf/NabfAgentLogic/logic/ExplorerLogic.fs
@@ -94,3 +94,88 @@ module ExplorerLogic =
             tryGo s.World.[rn.Head] s
         else
             (false,None)
+
+
+    type ZoneVertex = 
+        {   
+            Vertex          : Vertex
+            ControlValue    : int
+            Lock            : bool
+            HasAgent        : bool
+            Desire          : int
+        }
+
+    let buildZoneVertex (vertex:Vertex) =
+        {
+            Vertex = vertex
+            ControlValue = 2 + vertex.Edges.Count
+            Desire = 0
+            Lock = false
+            HasAgent = true
+        } : ZoneVertex
+
+    let isIsland (vertex:Vertex) =
+        vertex.Edges.Count = 1
+
+    let shouldZoneVertexLockBasedOnIsland (graph:Graph) (zoneV:ZoneVertex) =
+        let vl = getNeighbours zoneV.Vertex.Identifier graph
+        List.exists isIsland vl
+    
+    
+
+    let getZoneVertexNeighbour (graph:Graph) (zl:ZoneVertex list) (zoneV:ZoneVertex) =
+        let vl = getNeighbours zoneV.Vertex.Identifier graph
+        List.filter (fun zv -> (List.exists (fun v -> v.Identifier = zv.Vertex.Identifier) vl)) zl
+    
+    let shouldZoneVertexLock (graph:Graph) (zl:ZoneVertex list) (zoneV:ZoneVertex) =
+        if zoneV.ControlValue <= 3 || zoneV.Lock then true
+        else
+            let zneighbours = getZoneVertexNeighbour graph zl zoneV
+            List.exists (fun zn -> zn.ControlValue = 2) zneighbours
+
+    let calcControlValue (graph:Graph) (zl:ZoneVertex list) (zoneV:ZoneVertex) =
+        let init = if zoneV.HasAgent then 2 else 0
+        let vl = getNeighbours zoneV.Vertex.Identifier graph
+        let zbours = getZoneVertexNeighbour graph zl zoneV 
+        List.length (List.filter (fun zn -> zn.HasAgent) zbours)
+    
+    let calcDesire (graph:Graph) (zl:ZoneVertex list) (zoneV:ZoneVertex) =       
+        let zneighbours = getZoneVertexNeighbour graph zl zoneV
+        //IF Even -1; IF Uneven +1
+        let calcSingleDesire cval = (((cval % 2) * 2) - 1)
+        List.sum (List.map (fun zn -> calcSingleDesire zn.ControlValue ) zneighbours)
+    
+    let hasLockedNeighbour (graph:Graph) (zl:ZoneVertex list) (zoneV:ZoneVertex) =
+        let zneighbours = getZoneVertexNeighbour graph zl zoneV
+        List.exists (fun zn -> zn.Lock ) zneighbours
+
+    let choseRemove (graph:Graph) (zl:ZoneVertex list)= 
+        let sorted = List.sortBy (fun zv -> zv.Desire) zl
+        if List.isEmpty sorted then
+            None
+        else
+            let findNLocked = List.tryFind ( fun zn -> hasLockedNeighbour graph zl zn) sorted
+            if findNLocked.IsSome then
+                findNLocked
+            else
+                Some sorted.Head
+    
+    let rec calcAgentPositions (graph:Graph) (zl:ZoneVertex list) =
+        let zl = List.map (fun zn -> {zn with Lock = (shouldZoneVertexLock graph zl zn)}) zl
+        let zl = List.map (fun zn -> {zn with ControlValue = calcControlValue graph zl zn}) zl
+        let zl = List.map (fun zn -> {zn with Desire = calcDesire graph zl zn}) zl
+        let rz = choseRemove graph zl
+        match rz with
+        | None -> zl
+        | Some removed -> 
+            let zl =    {   removed with HasAgent = false}
+                        ::  (List.filter (fun zn -> zn.Vertex.Identifier <> removed.Vertex.Identifier ) zl)
+            calcAgentPositions graph zl 
+
+    let findAgentPlacement (subgraph:Graph) =
+        let vl = snd (List.unzip (Map.toList subgraph))
+        let zl = List.map buildZoneVertex vl
+        let zl = List.map (fun zn -> {zn with Lock = (shouldZoneVertexLockBasedOnIsland subgraph zn)}) zl
+        let zl = calcAgentPositions subgraph zl
+        let zl = List.filter (fun zn -> zn.HasAgent ) zl
+        List.map (fun zn -> zn.Vertex.Identifier) zl
