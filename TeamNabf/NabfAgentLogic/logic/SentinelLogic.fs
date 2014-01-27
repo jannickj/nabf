89633403bd8eb2133225b2d182699889b052b0db
PhilipOrum <philip.oerum@gmail.com>
2014-01-27 16:57:55 +0100

    Added occupy behavior to sentinels

--- TeamNabf/NabfAgentLogic/logic/SentinelLogic.fs
+++ TeamNabf/NabfAgentLogic/logic/SentinelLogic.fs
@@ -4,7 +4,27 @@ module SentinelLogic =
 
     open AgentTypes
     open AgentLogicLib
+    open PathFinding
 
     let sentinelReact (s:State) (agents:Agent list * Agent list) =
         let potentialEnemySabs = List.filter (fun a -> (a.Role = Some Saboteur || a.Role = None)) (fst agents)
         if not potentialEnemySabs.IsEmpty then tryDo Parry s else (false,None)
+
+    let rec findOccupyGoal (g:Goal list) =
+        match g with
+        | head :: tail -> 
+            match head with
+            | OccupyGoal(v) -> Some v
+            | _ -> findOccupyGoal tail
+        | [] -> None
+
+    let sentinelOccupy (s:State) =
+        match (findOccupyGoal s.Goals) with
+        | Some v ->
+            let goal = pathTo s.Self v s.World
+            match goal with
+            | Some vl -> tryGo s.World.[vl.Head] s
+            | None -> (false,None)
+        | None -> (false,None)
+
+    
